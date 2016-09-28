be713dc918c491e6d3df588694a9c8c7744adffd
masud.zaman <masud.zaman@sebpo.com>
2016-09-28 21:14:40 +0600

    Added allowed extension to configuration file

--- config/kindeditor.php
+++ config/kindeditor.php
@@ -7,5 +7,19 @@
  * Time: 16:48
  */
 return [
-	"url"=> "laravel-kindeditor"
+	'url' => 'laravel-kindeditor',
+	'support' => [
+			'image' => [ 'gif', 'jpg', 'jpeg', 'png', 'bmp',
+				'size' => 1024*1024*0.2],
+			'audio' => [ 'mp3', 'wav', 'wma', 'wmv', 'mid', 'avi',
+				'size' => 1024*1024*20],
+			'video' => [ 'mp4','mpg', 'asf', 'ogg', 'rmvb',
+				'size' => 1024*1024*100],
+			'flash' => [ 'rm', 'rmvb',
+				'size' => 1024*1024*20],
+			'media' => [ 'swf', 'flv', 'mp3', 'mp4', 'wav', 'wma', 'wmv', 'mid', 'avi', 'mpg', 'asf', 'rm', 'rmvb',
+				'size' => 1024*1024*200],
+			'file'  => [ 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'htm', 'html', 'txt', 'zip', 'rar', 'gz', 'bz2',
+				'size' => 1024*1024*2]
+		]
 ];
